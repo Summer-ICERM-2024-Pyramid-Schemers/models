bea1cbb1166c70cc5d87c870b6aa8b92d6b3afb8
b-orman <benjaminorman3@gmail.com>
2024-07-11 10:07:07 -0400

    Added Germany model comparisons

--- compareModels.py
+++ compareModels.py
@@ -9,7 +9,7 @@ from homeAdvModel import HomeAdvModel
 from transfermarktModel import TMModelOrderedProbit, TMModelOrderedProbitOLSGoalDiff
 from masseyModel import MasseyModel, WeightedMasseyModel
 
-def compareModels(getM1BrierScores, getM2BrierScores, excludel2 = False):
+def compareModels(getM1BrierScores, getM2BrierScores, country, excludel2 = False):
     """
     Returns a dataframe containing a list for each league and season.
 
@@ -21,42 +21,62 @@ def compareModels(getM1BrierScores, getM2BrierScores, excludel2 = False):
     [2] - A lower 95% confidence interval bound for Brier score difference
     [3] - An upper 95% confidence interval bound for Brier score difference
     """
-    comparison = pd.DataFrame(columns=['Year', 'Premier League','Championship','League One','League Two'])
+    if country.casefold() == "england":
+        comparison = pd.DataFrame(columns=['Year', 'Premier League','Championship','League One','League Two'])
+    elif country.casefold() == "germany":
+        comparison = pd.DataFrame(columns=['Year', 'Bundesliga', '2. Bundesliga'])
+    else:
+        raise Exception("Invalid country")
     for season in range(2012, 2024, 1):
         # For each league, calculate raw difference M1-M2 Brier score
         # Then calculate p value from t test
         # Then add lower and upper CIs
         # Add list to slot in table
-        b1 = getM1BrierScores(season, 1)
-        b2 = getM2BrierScores(season, 1)
-        ttest = stats.ttest_rel(b1,b2)
-        ci = ttest.confidence_interval()
-        prem = [np.mean(b1) - np.mean(b2), ttest[1], ci[0], ci[1]]
-        b1 = getM1BrierScores(season, 2)
-        b2 = getM2BrierScores(season, 2)
-        ttest = stats.ttest_rel(b1,b2)
-        ci = ttest.confidence_interval()
-        Ch = [np.mean(b1) - np.mean(b2), ttest[1], ci[0], ci[1]]
-        b1 = getM1BrierScores(season, 3)
-        b2 = getM2BrierScores(season, 3)
-        ttest = stats.ttest_rel(b1,b2)
-        ci = ttest.confidence_interval()
-        l1 = [np.mean(b1) - np.mean(b2), ttest[1], ci[0], ci[1]]
-        if excludel2 == False:
-            b1 = getM1BrierScores(season, 4)
-            b2 = getM2BrierScores(season, 4)
+        if country.casefold() == "england":
+            b1 = getM1BrierScores(season, 1)
+            b2 = getM2BrierScores(season, 1)
             ttest = stats.ttest_rel(b1,b2)
             ci = ttest.confidence_interval()
-            l2 = [np.mean(b1) - np.mean(b2), ttest[1], ci[0], ci[1]]
-        elif excludel2 == True:
-            l2 = [0,0,0,0]
-        comparison = pd.concat([pd.DataFrame([[season, prem, Ch, l1, l2]], 
-                                        columns=comparison.columns), comparison], 
-                                        ignore_index=True)
+            prem = [np.mean(b1) - np.mean(b2), ttest[1], ci[0], ci[1]]
+            b1 = getM1BrierScores(season, 2)
+            b2 = getM2BrierScores(season, 2)
+            ttest = stats.ttest_rel(b1,b2)
+            ci = ttest.confidence_interval()
+            Ch = [np.mean(b1) - np.mean(b2), ttest[1], ci[0], ci[1]]
+            b1 = getM1BrierScores(season, 3)
+            b2 = getM2BrierScores(season, 3)
+            ttest = stats.ttest_rel(b1,b2)
+            ci = ttest.confidence_interval()
+            l1 = [np.mean(b1) - np.mean(b2), ttest[1], ci[0], ci[1]]
+            if excludel2 == False:
+                b1 = getM1BrierScores(season, 4)
+                b2 = getM2BrierScores(season, 4)
+                ttest = stats.ttest_rel(b1,b2)
+                ci = ttest.confidence_interval()
+                l2 = [np.mean(b1) - np.mean(b2), ttest[1], ci[0], ci[1]]
+            elif excludel2 == True:
+                l2 = [0,0,0,0]
+            comparison = pd.concat([pd.DataFrame([[season, prem, Ch, l1, l2]], 
+                                            columns=comparison.columns), comparison], 
+                                            ignore_index=True)
+        elif country.casefold() == "germany":
+            b1 = getM1BrierScores(season, 5)
+            b2 = getM2BrierScores(season, 5)
+            ttest = stats.ttest_rel(b1,b2)
+            ci = ttest.confidence_interval()
+            bund1 = [np.mean(b1) - np.mean(b2), ttest[1], ci[0], ci[1]]
+            b1 = getM1BrierScores(season, 6)
+            b2 = getM2BrierScores(season, 6)
+            ttest = stats.ttest_rel(b1,b2)
+            ci = ttest.confidence_interval()
+            bund2 = [np.mean(b1) - np.mean(b2), ttest[1], ci[0], ci[1]]
+            comparison = pd.concat([pd.DataFrame([[season, bund1, bund2]], 
+                                            columns=comparison.columns), comparison], 
+                                            ignore_index=True)
     return comparison
 
-def plotComparison(getM1BrierScores, getM2BrierScores, M1Title, M2Title, excludel2 = False):
-    comparison = compareModels(getM1BrierScores, getM2BrierScores, excludel2)
+def plotComparison(getM1BrierScores, getM2BrierScores, M1Title, M2Title, country, excludel2 = False):
+    comparison = compareModels(getM1BrierScores, getM2BrierScores, country, excludel2)
     comparison = comparison.set_index("Year")
     diffs = comparison.applymap(lambda x: x[0])
     pvalues = comparison.applymap(lambda x: x[1])
@@ -89,7 +109,7 @@ def plotComparison(getM1BrierScores, getM2BrierScores, M1Title, M2Title, exclude
 
     plt.show()
 
-def compareModelsAggregate(getM1BrierScores, getM2BrierScores, excludel2 = False):
+def compareModelsAggregate(getM1BrierScores, getM2BrierScores, country, excludel2 = False):
     """
     Returns a data frame with a column for each league, with a list containing aggregate data
 
@@ -101,61 +121,88 @@ def compareModelsAggregate(getM1BrierScores, getM2BrierScores, excludel2 = False
     [2] - A lower 95% confidence interval bound for Brier score difference
     [3] - An upper 95% confidence interval bound for Brier score difference
     """
-    comparison = pd.DataFrame(columns=['Premier League','Championship','League One','League Two'])
-    l1m1 = []
-    l2m1 = []
-    l3m1 = []
-    l4m1 = []
-    l1m2 = []
-    l2m2 = []
-    l3m2 = []
-    l4m2 = []
-    for season in range(2012, 2024, 1):
-        # For each league, calculate raw difference M1-M2 Brier score
-        # Then calculate p value from t test
-        # Then add lower and upper CIs
-        # Add list to slot in table
-        l1m1.extend(getM1BrierScores(season, 1))
-        l1m2.extend(getM2BrierScores(season, 1))
+    
+    if country.casefold() == "england":
+        comparison = pd.DataFrame(columns=['Premier League','Championship','League One','League Two'])
+        l1m1 = []
+        l2m1 = []
+        l3m1 = []
+        l4m1 = []
+        l1m2 = []
+        l2m2 = []
+        l3m2 = []
+        l4m2 = []
+    elif country.casefold() == "germany":
+        comparison = pd.DataFrame(columns=['Bundesliga', '2. Bundesliga'])
+        l5m1 = []
+        l5m2 = []
+        l6m1 = []
+        l6m2 = []
+    else:
+        raise Exception("Invalid country")
+    
+    if country.casefold() == 'england':
+        for season in range(2012, 2024, 1):
+            l1m1.extend(getM1BrierScores(season, 1))
+            l1m2.extend(getM2BrierScores(season, 1))
 
-        l2m1.extend(getM1BrierScores(season, 2))
-        l2m2.extend(getM2BrierScores(season, 2))
+            l2m1.extend(getM1BrierScores(season, 2))
+            l2m2.extend(getM2BrierScores(season, 2))
 
-        l3m1.extend(getM1BrierScores(season, 3))
-        l3m2.extend(getM2BrierScores(season, 3))
+            l3m1.extend(getM1BrierScores(season, 3))
+            l3m2.extend(getM2BrierScores(season, 3))
+
+            if excludel2 == False:
+                l4m1.extend(getM1BrierScores(season, 4))
+                l4m2.extend(getM2BrierScores(season, 4))
+
+        ttest = stats.ttest_rel(l1m1, l1m2)
+        ci = ttest.confidence_interval()
+        prem = [np.mean(l1m1) - np.mean(l1m2), ttest[1], ci[0], ci[1]]
+
+        ttest = stats.ttest_rel(l2m1, l2m2)
+        ci = ttest.confidence_interval()
+        Ch = [np.mean(l2m1) - np.mean(l2m2), ttest[1], ci[0], ci[1]]
+
+        ttest = stats.ttest_rel(l3m1, l3m2)
+        ci = ttest.confidence_interval()
+        l1 = [np.mean(l3m1) - np.mean(l3m2), ttest[1], ci[0], ci[1]]
 
         if excludel2 == False:
-            l4m1.extend(getM1BrierScores(season, 4))
-            l4m2.extend(getM2BrierScores(season, 4))
+            ttest = stats.ttest_rel(l4m1, l4m2)
+            ci = ttest.confidence_interval()
+            l2 = [np.mean(l4m1) - np.mean(l4m2), ttest[1], ci[0], ci[1]]
+        elif excludel2 == True:
+            l2 = [0,0,0,0]
 
-    ttest = stats.ttest_rel(l1m1, l1m2)
-    ci = ttest.confidence_interval()
-    prem = [np.mean(l1m1) - np.mean(l1m2), ttest[1], ci[0], ci[1]]
+        comparison = pd.concat([pd.DataFrame([[prem, Ch, l1, l2]], 
+                                            columns=comparison.columns), comparison], 
+                                            ignore_index=True)
+    elif country.casefold() == 'germany':
+        for season in range(2012, 2024, 1):
+            l5m1.extend(getM1BrierScores(season, 5))
+            l5m2.extend(getM2BrierScores(season, 5))
 
-    ttest = stats.ttest_rel(l2m1, l2m2)
-    ci = ttest.confidence_interval()
-    Ch = [np.mean(l2m1) - np.mean(l2m2), ttest[1], ci[0], ci[1]]
+            l6m1.extend(getM1BrierScores(season, 6))
+            l6m2.extend(getM2BrierScores(season, 6))
 
-    ttest = stats.ttest_rel(l3m1, l3m2)
-    ci = ttest.confidence_interval()
-    l1 = [np.mean(l3m1) - np.mean(l3m2), ttest[1], ci[0], ci[1]]
-
-    if excludel2 == False:
-        ttest = stats.ttest_rel(l4m1, l4m2)
+        ttest = stats.ttest_rel(l5m1, l5m2)
         ci = ttest.confidence_interval()
-        l2 = [np.mean(l4m1) - np.mean(l4m2), ttest[1], ci[0], ci[1]]
-    elif excludel2 == True:
-        l2 = [0,0,0,0]
+        bund1 = [np.mean(l5m1) - np.mean(l5m2), ttest[1], ci[0], ci[1]]
 
-    comparison = pd.concat([pd.DataFrame([[prem, Ch, l1, l2]], 
-                                        columns=comparison.columns), comparison], 
-                                        ignore_index=True)
+        ttest = stats.ttest_rel(l6m1, l6m2)
+        ci = ttest.confidence_interval()
+        bund2 = [np.mean(l6m1) - np.mean(l6m2), ttest[1], ci[0], ci[1]]
+
+        comparison = pd.concat([pd.DataFrame([[bund1, bund2]],
+                                            columns=comparison.columns), comparison], 
+                                            ignore_index=True)
     return comparison
 
 
-def plotAggregateComparison(getM1BrierScores, getM2BrierScores, M1Title, M2Title, excludel2 = False):
-    comparison = compareModelsAggregate(getM1BrierScores, getM2BrierScores, excludel2)
-    print(comparison)
+def plotAggregateComparison(getM1BrierScores, getM2BrierScores, M1Title, M2Title, country, excludel2 = False):
+    comparison = compareModelsAggregate(getM1BrierScores, getM2BrierScores, country, excludel2)
+
     diffs = comparison.applymap(lambda x: x[0])
     pvalues = comparison.applymap(lambda x: x[1])
     lowerCI = comparison.applymap(lambda x: x[2])
@@ -187,7 +234,7 @@ def plotAggregateComparison(getM1BrierScores, getM2BrierScores, M1Title, M2Title
 
     plt.show()
 
-def compareSRAggregate(getM1SuccessRatio, getM2SuccessRatio, excludel2 = False):
+def compareSRAggregate(getM1SuccessRatio, getM2SuccessRatio, country, excludel2 = False):
     """
     Returns a data frame with a column for each league, with a list containing aggregate data
 
@@ -199,37 +246,63 @@ def compareSRAggregate(getM1SuccessRatio, getM2SuccessRatio, excludel2 = False):
     [2] - A lower 95% confidence interval bound for Brier score difference
     [3] - An upper 95% confidence interval bound for Brier score difference
     """
-    comparison = pd.DataFrame(columns=['Premier League','Championship','League One','League Two'])
 
-    l1m1, l1m2, l2m1, l2m2, l3m1, l3m2, l4m1, l4m2 = 0,0,0,0,0,0,0,0
-    for season in range(2012, 2024, 1):
-        div = season - 2011
-        mult = season - 2012
-        # Each proportion is of the same number of games...
-        l1m1 = (getM1SuccessRatio(season, 1) + (l1m1 * mult)) / div
-        l1m2 = (getM2SuccessRatio(season, 1) + (l1m2 * mult)) / div
-        l2m1 = (getM1SuccessRatio(season, 2) + (l2m1 * mult)) / div
-        l2m2 = (getM2SuccessRatio(season, 2) + (l2m2 * mult)) / div
-        l3m1 = (getM1SuccessRatio(season, 3) + (l3m1 * mult)) / div
-        l3m2 = (getM2SuccessRatio(season, 3) + (l3m2 * mult)) / div
-        l4m1 = (getM1SuccessRatio(season, 4) + (l4m1 * mult)) / div
-        l4m2 = (getM2SuccessRatio(season, 4) + (l4m2 * mult)) / div
-    
-    prem = l1m1 - l1m2
-    Ch = l2m1 - l2m2
-    l1 = l3m1 - l3m2
-    if excludel2 == False:
-        l2 = l4m1 - l4m2
-    elif excludel2 == True:
-        l2 = 0
+    if country.casefold() == "england":
+        comparison = pd.DataFrame(columns=['Premier League','Championship','League One','League Two'])
+        l1m1, l1m2, l2m1, l2m2, l3m1, l3m2, l4m1, l4m2 = 0,0,0,0,0,0,0,0
+    elif country.casefold() == "germany":
+        comparison = pd.DataFrame(columns=['Bundesliga', '2. Bundesliga'])
+        l5m1, l5m2, l6m1, l6m2 = 0,0,0,0
+    else:
+        raise Exception("Invalid country")
 
-    comparison = pd.concat([pd.DataFrame([[prem, Ch, l1, l2]], 
-                                        columns=comparison.columns), comparison], 
-                                        ignore_index=True)
+    if country.casefold() == "england":
+        for season in range(2012, 2024, 1):
+            div = season - 2011
+            mult = season - 2012
+            # Each proportion is of the same number of games...
+            l1m1 = (getM1SuccessRatio(season, 1) + (l1m1 * mult)) / div
+            l1m2 = (getM2SuccessRatio(season, 1) + (l1m2 * mult)) / div
+            l2m1 = (getM1SuccessRatio(season, 2) + (l2m1 * mult)) / div
+            l2m2 = (getM2SuccessRatio(season, 2) + (l2m2 * mult)) / div
+            l3m1 = (getM1SuccessRatio(season, 3) + (l3m1 * mult)) / div
+            l3m2 = (getM2SuccessRatio(season, 3) + (l3m2 * mult)) / div
+            l4m1 = (getM1SuccessRatio(season, 4) + (l4m1 * mult)) / div
+            l4m2 = (getM2SuccessRatio(season, 4) + (l4m2 * mult)) / div
+        
+        prem = l1m1 - l1m2
+        Ch = l2m1 - l2m2
+        l1 = l3m1 - l3m2
+        if excludel2 == False:
+            l2 = l4m1 - l4m2
+        elif excludel2 == True:
+            l2 = 0
+
+        comparison = pd.concat([pd.DataFrame([[prem, Ch, l1, l2]], 
+                                            columns=comparison.columns), comparison], 
+                                            ignore_index=True)
+        
+    elif country.casefold() == "germany":
+        for season in range(2012, 2024, 1):
+            div = season - 2011
+            mult = season - 2012
+            # Each proportion is of the same number of games...
+            l5m1 = (getM1SuccessRatio(season, 5) + (l5m1 * mult)) / div
+            l5m2 = (getM2SuccessRatio(season, 5) + (l5m2 * mult)) / div
+            l6m1 = (getM1SuccessRatio(season, 6) + (l6m1 * mult)) / div
+            l6m2 = (getM2SuccessRatio(season, 6) + (l6m2 * mult)) / div
+        
+        bund1 = l5m1 - l5m2
+        bund2 = l6m1 - l6m2
+
+        comparison = pd.concat([pd.DataFrame([[bund1, bund2]], 
+                                            columns=comparison.columns), comparison], 
+                                            ignore_index=True)
+
     return comparison
 
-def plotSRAggregateComparison(getM1SuccessRatio, getM2SuccessRatio, M1Title, M2Title, excludel2 = False):
-    diffs = compareSRAggregate(getM1SuccessRatio, getM2SuccessRatio, excludel2)
+def plotSRAggregateComparison(getM1SuccessRatio, getM2SuccessRatio, M1Title, M2Title, country, excludel2 = False):
+    diffs = compareSRAggregate(getM1SuccessRatio, getM2SuccessRatio, country, excludel2)
     print(diffs)
     
     df_combined = diffs.stack().reset_index()
@@ -249,8 +322,8 @@ def plotSRAggregateComparison(getM1SuccessRatio, getM2SuccessRatio, M1Title, M2T
     plt.show()
 
 # Odds - HA
-#plotComparison(BettingOddsModel.getBrierScores, HomeAdvModel.getBrierScores, "Betting Odds", "Home Advantage")
-#plotAggregateComparison(BettingOddsModel.getBrierScores, HomeAdvModel.getBrierScores, "Betting Odds", "Home Advantage")
+#plotComparison(BettingOddsModel.getBrierScores, HomeAdvModel.getBrierScores, "Betting Odds", "Home Advantage", "Germany")
+plotAggregateComparison(BettingOddsModel.getBrierScores, HomeAdvModel.getBrierScores, "Betting Odds", "Home Advantage", "Germany")
 
 # Odds - TM1
 #plotComparison(BettingOddsModel.getBrierScores, TMModelOrderedProbit.getBrierScores, "Betting Odds", "Transfermarkt Model 1")
@@ -267,5 +340,3 @@ def plotSRAggregateComparison(getM1SuccessRatio, getM2SuccessRatio, M1Title, M2T
 # TM2 - HA
 #plotComparison(TMModelOrderedProbitOLSGoalDiff.getBrierScores, HomeAdvModel.getBrierScores, "Transfermarkt Model 2", "Home Advantage")
 #plotAggregateComparison(TMModelOrderedProbitOLSGoalDiff.getBrierScores, HomeAdvModel.getBrierScores, "Transfermarkt Model 2", "Home Advantage")
-
-plotSRAggregateComparison(BettingOddsModel.getSuccessRatio, TMModelOrderedProbit.getSuccessRatio, "Betting Odds", "Transfermarkt Model 1")
