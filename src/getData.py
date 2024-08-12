069a5643ebb0a2de86cf5242730cdd0c909f8ba8
BonelessPi <84295882+BonelessPi@users.noreply.github.com>
2024-08-12 16:38:36 -0400

    Move team_id exclusion to a kwarg for fetch_data_for_in_season_brier

--- src/getData.py
+++ src/getData.py
@@ -221,7 +221,7 @@ def fetch_data_for_massey_accuracy(season,league):
     return Games
 
 
-def fetch_data_for_in_season_brier(season, league):
+def fetch_data_for_in_season_brier(season, league, *, exclude_team_ids=[]):
     '''
     return 1) first 80% data in each season for transfermarkt models & rank models
             2) last 20% for evaluation
@@ -266,19 +266,11 @@ def fetch_data_for_in_season_brier(season, league):
     tmk_games = pd.read_sql_query(tmkQuery, con)
     tmk_games = tmk_games.query(f'season == {season} and league_id == {league}').copy()
 
-    # exclud top teams 
-    top_teams=[129, 130]
-    #top_teams = [93, 94, 95]
 
-
-    rank_mask = ~((rank_games['home_team_id'].isin(top_teams)) | (rank_games['away_team_id'].isin(top_teams)))
+    rank_mask = ~((rank_games['home_team_id'].isin(exclude_team_ids)) | (rank_games['away_team_id'].isin(exclude_team_ids)))
     rank_games = rank_games.loc[rank_mask, :].copy()
-    tmk_mask = ~((tmk_games['home_team_id'].isin(top_teams)) | (tmk_games['away_team_id'].isin(top_teams)))
+    tmk_mask = ~((tmk_games['home_team_id'].isin(exclude_team_ids)) | (tmk_games['away_team_id'].isin(exclude_team_ids)))
     tmk_games = tmk_games.loc[tmk_mask, :].copy()
-    '''
-    rank_games = rank_games[~((rank_games['home_team_id'].isin(top_teams)) | (rank_games['away_team_id'].isin(top_teams)))]
-    tmk_games = tmk_games[~((rank_games['home_team_id'].isin(top_teams)) | (tmk_games['away_team_id'].isin(top_teams)))]
-    '''
 
     rank_games['result'] = np.sign(rank_games["fulltime_home_goals"] - rank_games["fulltime_away_goals"]).astype(int)
 
